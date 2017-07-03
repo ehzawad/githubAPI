880256c06d1f657e854f674cca7b576e00f85c97
ehzawad <ehzawad@gmail.com>
2017-07-04 02:08:30 +0600

    toggling

--- src/Data.js
+++ src/Data.js
@@ -1,57 +1,71 @@
-import React from 'react';
+import React from "react";
 
 function ListItem(props) {
   // Correct! There is no need to specify the key here:
   return (
     <div>
       <li>
-        <span style={{color: "purple"}}>{props.reponame}</span> <strong>-></strong>
-        <span style={{color: "brown"}}> {props.description}</span>
+        <span style={{ color: "purple" }}>{props.reponame}</span>{" "}
+        <strong>-></strong>
+        <span style={{ color: "brown" }}> {props.description}</span>
       </li>
-
     </div>
-
-  )
+  );
 }
 
 class MyComponent extends React.Component {
-
   constructor(props) {
-    super(props)
-    this.state = {data: []}
+    super(props);
+    this.state = { data: [], gistsORrepos: "gists" };
+
+    // this binding is necessary to make `this` work in the callback
+    this.handleClick = this.handleClick.bind(this);
+    this.fetching = this.fetching.bind(this);
+  }
+
+  handleClick() {
+    this.setState(prevState => ({
+      gistsORrepos: prevState.gistsORrepos === "gists" ? "repos" : "gists"
+    }));
+    this.fetching()
+    console.log(this.state.gistsORrepos)
+  }
+
+  fetching() {
+    let USER = "abrarshariar";
+
+    const URL = `https://api.github.com/users/${USER}/${this.state.gistsORrepos}`;
+    let f = fetch(URL);
+    f
+      .then(response => response.json())
+      .then(responseJson => {
+        this.setState({ data: responseJson });
+      })
+      .catch(error => {
+        console.error(error);
+      });
   }
 
   componentDidMount() {
-
-    let USER = 'abrarshariar'
-    let gistsORrepos = 'repos'
-    const URL = `https://api.github.com/users/${USER}/${gistsORrepos}`
-    let f = fetch(URL)
-    f.then((response) => response.json())
-    .then((responseJson) => {
-      this.setState({data: responseJson})
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+      this.fetching()
   }
 
   render() {
     const jsonData = this.state.data;
-    console.log(jsonData)
-
+    // console.log(jsonData)
     // jsonData.map(x => console.log(x.name))
-     const listItems = jsonData.map((x, index) =>
-      <ListItem key={index}
-        reponame={x.name} description={x.description}
-      />
+    const listItems = jsonData.map((x, index) =>
+      <ListItem key={index} reponame={x.name} description={x.description} />
     );
 
     return (
       <div>
+        <button onClick={this.handleClick}>
+          {this.state.gistsORrepos ==="repos" ? "gists" : "repos"}
+        </button>
         {listItems}
       </div>
-    )
+    );
   }
 }
 
